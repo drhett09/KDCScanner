6c903fb2f8f66b39cf51b90f0029b0e33325be04
drhett09 <drhett09@gmail.com>
2026-02-16 17:33:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = "scanner-cache-v1";
-
+const CACHE_NAME = "scanner-cache-v2";
 const urlsToCache = [
   "index.html",
   "opencv.js",
   "jspdf.umd.min.js",
-  "tesseract.min.js",
-  "docx.min.js"
+  "tesseract.min.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
